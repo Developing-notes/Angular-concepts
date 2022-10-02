e02352e4768176ba454d7a7e3d5123d5519c79bd
Developing-notes <101121374+Developing-notes@users.noreply.github.com>
2022-10-02 22:17:37 +0530

    update

--- files/interval&Subscription.ts
+++ files/interval&Subscription.ts
@@ -6,31 +6,45 @@ import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-hello',
-  template: `<h1>{{ content }}</h1>
+  template: `<h1>{{ content1 }}
+    <br>{{content2}}</h1>
    <!-- <button (click)="stopTimer()">stopTimer</button> -->
   `,
   styleUrls: ['./hello.component.css'],
 })
 export class HelloComponent implements OnInit {
   mySubscription: Subscription
-  content: any
+  content1: string;
+  content2: any
   ngOnInit() {
     console.log('this is oninit');
+    this.setInterval()    
+  }
+
+  setInterval(){
     this.text()
     this.mySubscription = interval(5000).subscribe((x => {
       console.log("x: ", x);
-      this.stop();
+      this.clearInterval();
     })
     )
   }
 
-  text() {
-    this.content = "hello"
-  }
-  stop() {
+  clearInterval() {
     //doing stuff with unsubscribe at end to only run once
     this.mySubscription.unsubscribe();
-    this.content = "helloworld"
+    this.changeText()
   }
+  
+  text() {
+    this.content1 = "hello"
+    this.content2 = "welcome"
+  }
+
+  changeText() {
+    this.content1 = "hello world"
+    this.content2 = "welcome world"
+  }
+
 
 }
